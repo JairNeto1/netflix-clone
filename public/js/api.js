cf13c966b7f934f13d256f9d816316f02c9abfb2
JairNeto1 <jairmonteironeto@gmail.com>
2021-09-19 20:11:55 -0300

    updating

--- public/js/api.js
+++ public/js/api.js
@@ -1,9 +1,5 @@
 const fetch = require('fetch')
 
-const base_url = 'https://api.themoviedb.org/3/'
-const api_key = 'api_key=a06f6de12c4b33db6e6e9d0ccd82bb20'
-const best_rated_api = base_url + 'movie/top_rated?' + api_key
-
 getBestRated(best_rated_api)
 
 function getBestRated(url) {
